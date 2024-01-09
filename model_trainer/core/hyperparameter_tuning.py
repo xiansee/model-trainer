9cb54c72518e59bd663bd36dd4d4b58094f38347
Xian <xian.see@outlook.com>
2024-01-09 08:56:38 -0500

    Refactored hyperparameter tuning

--- model_trainer/core/hyperparameter_tuning.py
+++ model_trainer/core/hyperparameter_tuning.py
@@ -1,7 +1,9 @@
 from typing import Any, Callable
 
+import lightning as L
 import lightning.pytorch as pl
 import optuna
+from lightning.pytorch.callbacks import Callback
 from optuna.trial import Trial
 from torch import Tensor
 
@@ -10,6 +12,7 @@ from model_trainer.core.hyperparameter import (
     FloatHyperparameter,
     IntegerHyperparameter,
 )
+from model_trainer.core.logging import Logger
 from model_trainer.core.training import TrainingModule
 from model_trainer.core.training_config import TrainingConfig
 
@@ -61,7 +64,79 @@ def define_module(trial: Trial, module: Any, module_init_params: dict) -> Any:
     return module(**processed_init_params)
 
 
-def get_objective_function(training_config: TrainingConfig) -> Callable:
+def initialize_trial(
+    training_config: TrainingConfig, trial: Trial, callbacks: list[Callback] = []
+) -> tuple[pl.Trainer, TrainingModule, L.LightningDataModule]:
+    """
+    Initialize hyperparameter tuning trial based on Optuna framework.
+
+    Parameters
+    ----------
+    training_config : TrainingConfig
+        User defined training configuration
+    trial : Trial
+        Optuna study trial
+    callbacks : list[Callback]
+        List of callbacks for Lightning trainer
+
+    Returns
+    -------
+    tuple[pl.Trainer, TrainingModule, L.LightningDataModule]
+        Initialized modules for fitting
+    """
+
+    # Init model
+    model_config = training_config.model
+    model_class = model_config.model
+    model_init_params = dict(model_config)
+    model = define_module(
+        trial=trial, module=model_class, module_init_params=model_init_params
+    )
+
+    # Init data module
+    data_module_config = training_config.data_module
+    data_module_class = data_module_config.data_module
+    data_module_init_params = dict(data_module_config)
+    data_module = define_module(
+        trial=trial,
+        module=data_module_class,
+        module_init_params=data_module_init_params,
+    )
+
+    # Init optimizer
+    optimizer_config = training_config.optimizer
+    optimizer_class = optimizer_config.optimizer_algorithm
+    optimizer_init_params = dict(optimizer_config)
+    optimizer_init_params.update({"params": model.parameters()})
+    optimizer = define_module(
+        trial=trial,
+        module=optimizer_class,
+        module_init_params=optimizer_init_params,
+    )
+
+    # Init training module
+    training_module = TrainingModule(
+        model=model,
+        optimizer=optimizer,
+        loss_function=training_config.trainer.loss_function,
+    )
+
+    # Init trainer
+    trainer = pl.Trainer(
+        # default_root_dir="../lightining_logs",
+        max_epochs=training_config.max_epochs,
+        max_time={"minutes": training_config.max_time},
+        logger=False,
+        callbacks=callbacks,
+        enable_progress_bar=False,
+        enable_model_summary=False,
+        enable_checkpointing=False,
+    )
+
+    return trainer, training_module, data_module
+
+
+def get_objective_function(training_config: TrainingConfig, logger: Logger) -> Callable:
     """
     Get objective function for Optuna study.
 
@@ -69,6 +144,8 @@ def get_objective_function(training_config: TrainingConfig) -> Callable:
     ----------
     training_config : TrainingConfig
         User defined training configuration
+    logger : Logger
+        Hyperparameter tuning logger
 
     Returns
     -------
@@ -79,57 +156,13 @@ def get_objective_function(training_config: TrainingConfig) -> Callable:
     def objective(trial: Trial) -> Tensor:
         """Objective function for Optuna study."""
 
-        # Init model
-        model_config = training_config.model
-        model_class = model_config.model
-        model_init_params = dict(model_config)
-        model = define_module(
-            trial=trial, module=model_class, module_init_params=model_init_params
-        )
+        with logger.start_trial_logs(trial_number=trial.number):
+            trainer, training_module, data_module = initialize_trial(
+                training_config=training_config, trial=trial, callbacks=[logger]
+            )
+            trainer.fit(training_module, data_module)
+            validation_accuracy = trainer.logged_metrics.get("validation_accuracy")
 
-        # Init data module
-        data_module_config = training_config.data_module
-        data_module_class = data_module_config.data_module
-        data_module_init_params = dict(data_module_config)
-        data_module = define_module(
-            trial=trial,
-            module=data_module_class,
-            module_init_params=data_module_init_params,
-        )
-
-        # Init optimizer
-        optimizer_config = training_config.optimizer
-        optimizer_class = optimizer_config.optimizer_algorithm
-        optimizer_init_params = dict(optimizer_config)
-        optimizer_init_params.update({"params": model.parameters()})
-        optimizer = define_module(
-            trial=trial,
-            module=optimizer_class,
-            module_init_params=optimizer_init_params,
-        )
-
-        # Init training module
-        training_module = TrainingModule(
-            model=model,
-            optimizer=optimizer,
-            loss_function=training_config.trainer.loss_function,
-        )
-
-        # Init trainer
-        trainer = pl.Trainer(
-            default_root_dir="../lightining_logs",
-            max_epochs=training_config.max_epochs,
-            max_time={"minutes": training_config.max_time},
-            logger=False,
-            callbacks=[],
-            enable_progress_bar=False,
-            enable_model_summary=False,
-            enable_checkpointing=False,
-        )
-
-        trainer.fit(training_module, data_module)
-
-        validation_accuracy = trainer.logged_metrics.get("validation_accuracy")
         return validation_accuracy
 
     return objective
@@ -137,7 +170,8 @@ def get_objective_function(training_config: TrainingConfig) -> Callable:
 
 def run_hyperparameter_tuning(training_config: TrainingConfig) -> None:
     """
-    Start hyperparameter tuning using Optuna framework.
+    Start hyperparameter tuning using Optuna framework and log training using MLflow
+    framework.
 
     Parameters
     ----------
@@ -145,13 +179,21 @@ def run_hyperparameter_tuning(training_config: TrainingConfig) -> None:
         User defined training configuration
     """
 
-    objective = get_objective_function(training_config=training_config)
-    study = optuna.create_study(
-        direction="minimize",
-        pruner=optuna.pruners.MedianPruner(
-            n_startup_trials=10, n_warmup_steps=20, interval_steps=5
-        ),
+    logger = Logger(
+        experiment=training_config.experiment,
+        run_name=training_config.run_name,
+        experiment_tags=training_config.experiment_tags,
     )
 
-    study.optimize(objective, n_trials=training_config.num_trials)
+    objective = get_objective_function(training_config=training_config)
+
+    with logger.start_hyperparameter_tuning_logs():
+        study = optuna.create_study(
+            direction="minimize",
+            pruner=optuna.pruners.MedianPruner(
+                n_startup_trials=10, n_warmup_steps=20, interval_steps=5
+            ),
+        )
+        study.optimize(objective, n_trials=training_config.num_trials)
+
     return study
